d0503d7f337ed7d28307f5ef45e02da6e8b10888
Jesse Schimmel <jhowardschimmel@gmail.com>
2020-02-04 23:51:18 -0500

    Change loop initializer to 0

--- looping-a-triangle.js
+++ looping-a-triangle.js
@@ -10,7 +10,7 @@ Write a loop that makes seven calls to console.log to output the following trian
 ####### */
 
 let triangleString = "";
-for (let i = 1; i < 8; i++) {
+for (let i = 0; i < 7; i++) {
     triangleString+="#";
     console.log(triangleString);   
 }
